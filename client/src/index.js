825831e80b0b5152a14323c0d1edc1bb61e1c714
treva556 <marktreva92@gmail.com>
2023-03-29 01:24:25 +0300

    structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-// import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
-import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
-import './index.css';
-import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import App from '../src/components/App'
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-const root = document.getElementById('root'); // <- This is the correct method call for React version 17
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  root
-);
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Router>
+      <Route path="/" component={App}/>
+    </Router>,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
