754ca593e72ddd64c5a834ba25e7a269cadbfbd8
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2023-03-29 11:56:36 +0300

    Added seed data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Booking.create([{flight_id: 1, passenger_name: "John Doe", passenger_email: "john.doe@example.com"}, {flight_id: 2, passenger_name: "Jane Smith", passenger_email: "jane.smith@example.com"}, {flight_id: 3, passenger_name: "Bob Johnson", passenger_email: "bob.johnson@example.com"}])
