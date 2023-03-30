fb419a2320444676af4059b80723af802f491aa2
icika-max <icikakennedy@gmail.com>
2023-03-30 22:15:37 +0300

    adding forgot password using devise gem

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # resources :airports
   # resources :reviews
   # resources :bookings, only: []
@@ -10,12 +11,37 @@ Rails.application.routes.draw do
   get '/bookings', to: 'bookings#index'
   post '/bookings', to: 'bookings#create'
   delete '/bookings/:id', to: 'bookings#destroy'
+  get '/flights/:flight_id/reviews', to: 'reviews#index'
   post '/flights/:flight_id/reviews', to: 'reviews#create'
   put '/flights/:flight_id/reviews/:id', to: 'reviews#update'
   delete '/flights/:flight_id/reviews/:id', to: 'reviews#destroy'
   get '/reviews/:id', to: 'reviews#show'
   post '/users', to: 'users#create'
   get '/users', to: 'users#index'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'reset_password', confirmation: 'verification', unlock: 'unlock_account', registration: 'signup', sign_up: '' }, controllers: { passwords: 'passwords' }
+
+ 
+  
+  
+
+  #This will add the following routes:
+  # Forget password routes
+# new_user_password GET    /reset_password/new(.:format)      passwords#new
+# edit_user_password GET    /reset_password/edit(.:format)     passwords#edit
+#      user_password PATCH  /reset_password(.:format)          passwords#update
+#                    PUT    /reset_password(.:format)          passwords#update
+#                    POST   /reset_password(.:format)          passwords#create
+
+# Reset password routes
+# cancel_user_registration GET    /signup/cancel(.:format)          registrations#cancel
+#    user_registration POST   /signup(.:format)                  registrations#create
+# new_user_registration GET    /signup/sign_up(.:format)          registrations#new
+# edit_user_registration GET    /signup/edit(.:format)             registrations#edit
+#                      PATCH  /signup(.:format)                  registrations#update
+#                      PUT    /signup(.:format)                  registrations#update
+#                      DELETE /signup(.:format)                  registrations#destroy
+
+
   
 
   
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_115046) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_184747) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -64,6 +64,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_115046) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "flights"
