c5ccbd93497b5f9021c9b160cddb589baafe325c
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-31 09:49:28 +0300

    added useeffect to fetch

--- client/src/components/Flights.js
+++ client/src/components/Flights.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Flight from './Flight';
 
 function Flights() {
   const [search, setSearch] = useState('');
   const [filter, setFilter] = useState('Departure');
+  const [flights, setFlights] = useState([]);
+
+  useEffect(()=>{
+    fetch('/flights')
+    .then(resp=>resp.json())
+    .then(data=>{
+      setFlights([...data])
+    })
+  },[])
 
   const handleSearchChange = (event) => {
     setSearch(event.target.value);
@@ -13,16 +22,17 @@ function Flights() {
     setFilter(event.target.value);
   };
 
-  const filteredFlights = FLIGHTS.filter(
+  const filteredFlights = flights?.filter(
     (flight) =>
-      filter === 'Departure'
-        ? flight.departureAirport.toLowerCase().includes(search.toLowerCase())
-        : flight.arrivalAirport.toLowerCase().includes(search.toLowerCase())
+      // filter === 'Departure'
+      //   ? flight.departureAirport.toLowerCase().includes(search.toLowerCase())
+      //   : flight.arrivalAirport.toLowerCase().includes(search.toLowerCase())
+      true
   );
 
   return (
     <div>
-      <form>
+      <form className='container my-3 p-4 border rounded shadow-sm bg-light'>
         <div className="form-group">
           <label htmlFor="search">Search:</label>
           <input
@@ -46,12 +56,13 @@ function Flights() {
           </select>
         </div>
       </form>
-      <div className="row row-cols-1 row-cols-md-3 g-4">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-1 mx-5 my-5 border rounded shadow-lg bg-light p-3">
         {filteredFlights.map((flight) => (
           <div className="col" key={flight.id}>
             <Flight
               departureAirport={flight.departureAirport}
               arrivalAirport={flight.arrivalAirport}
+              flightImage = {flight.image_url}
             />
           </div>
         ))}
