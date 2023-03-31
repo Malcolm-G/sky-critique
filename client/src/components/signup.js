0214aa2bec742c8a624d14d1f3ee42e4013d6712
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-31 19:40:47 +0300

    added signup functionality

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import {  useNavigate} from 'react-router-dom';
+import { UserContext } from './UserDataProvider';
 
 function Signup() {
     const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     // const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
+    const [user,setUser] = useContext(UserContext)
+    const navigate = useNavigate()
+    const input = {"name":userName, "email":email, "password":password, "password_confirmation":confirmPassword}
+    console.log(input)
+
+  function handleSubmit (event) {
     event.preventDefault();
-    // Your signup logic goes here
-    // navigate('/home');
+    fetch('/signup',{
+      method:'POST',
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(input)
+    })
+    .then(resp=>resp.json())
+    .then(data=>{
+      console.log(data)
+      if(!data.errors){
+        setUser(data)
+        navigate('/home');
+      }
+    })
   };
 
   return (
     <div className='form my-5'> 
       <h1>Sign Up</h1>
-      <form >
+      <form onSubmit={handleSubmit}>
         <label>
           Email:
           <input type="email" 
@@ -25,17 +45,24 @@ function Signup() {
         </label>
         <br />
         <label>
+          Username:
+          <input type="text" 
+          onChange={(e)=>setUserName(e.target.value)}
+          value={userName}/>
+        </label>
+        <br />
+        <label>
           Password:
           <input type="password"
-          onChange={(e)=>setConfirmPassword(e.target.value)}
-          value={confirmPassword}/>
+          onChange={(e)=>setPassword(e.target.value)}
+          value={password}/>
         </label>
         <br />
         <label>
           Confirm Password:
           <input type="password"
-          onChange={(e)=>setPassword(e.target.value)}
-          value={password}/>
+          onChange={(e)=>setConfirmPassword(e.target.value)}
+          value={confirmPassword}/>
         </label>
         <br />
         <button type="submit">Sign Up</button>
