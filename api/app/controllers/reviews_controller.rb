d672501e54672cae18a55c2f8652edfc96de3994
icika-max <icikakennedy@gmail.com>
2023-04-01 12:48:57 +0300

    modifying reviews  flight/routes

--- api/app/controllers/reviews_controller.rb
+++ api/app/controllers/reviews_controller.rb
@@ -27,8 +27,32 @@ class ReviewsController < ApplicationController
     end
     def index
       @flight = Flight.find(params[:flight_id])
-      @reviews = @flight.reviews
+      @reviews = @flight.reviews.includes(:user)
+      render json: @reviews.map { |review| review.as_json.except('user_id').merge(user_name: review.user.name) }
     end
+    def flight_reviews
+      @flight = Flight.find(params[:flight_id])
+      @reviews = @flight.reviews.includes(:user)
+      render json: { flight: @flight.as_json(only: [:id, :name]), route: @flight.route.as_json(only: [:id, :origin_name, :destination_name]), reviews: @reviews.as_json(except: [:user_id], methods: [:user_name]) }
+    end
+    
+    
+    
+    # def index
+    #   @flight = Flight.find(params[:flight_id])
+    #   @reviews = @flight.reviews
+    #   render json: @reviews
+    # end
+    # def show
+    #   @review = Review.find(params[:id])
+    #   render json: { 
+    #     id: @review.id, 
+    #     content: @review.content, 
+    #     rating: @review.rating, 
+    #     flight_id: @review.flight_id, 
+    #     user_name: @review.user.name 
+    #   }, status: :ok
+    # end
   
     private
   
